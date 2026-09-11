5bd0256838751043d83a4fe2e2cfd1821a783f18
9zdbcm692f-hash <9zdbcm692f@privaterelay.appleid.com>
2026-09-11 16:34:12 +0530

    day 1 of coding out of 100 days

---
+++ day1.c
@@ -0,0 +1,34 @@
+question 1
+#include<stdio.h>
+int main(){
+int a,b;
+  printf("enter value of a=");
+  scanf("%d",&a);
+  printf("enter value of b=");
+  scanf("%d",&b):
+    int sum;
+  sum=a+b;
+  printf("%d",sum);
+  return 0;
+}
+
+
+question 2
+#include<stdio.h>
+int main(){
+  int a,b;
+printf("enter value of a=");
+scanf("%d",&a);
+printf("enter value of b=");
+scanf("%d",&b);
+int sum,difference,product,quiotent;
+sum=a+b;
+difference=a-b;
+product=a*b;
+quiotent=a%b;
+printf("%d",sum);
+printf("\n%d",difference);
+printf("\n%d",product);
+printf("\n%d",quiotent);
+return 0;
+}
